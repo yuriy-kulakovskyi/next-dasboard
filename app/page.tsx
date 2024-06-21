d3af47fc4a6f35734ec86d4c55f5bcc516d6b067
yuriy-kulakovskyi <yurakulak42@gmail.com>
2024-06-21 22:23:43 +0300

    fixed styles of the image on the small screens on the login page

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Page() {
             src="/hero-mobile.png"
             width={560}
             height={620}
-            className='block md:block'
+            className='block md:hidden'
             alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
